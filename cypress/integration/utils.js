41c4751d830968b5d396984b0fa16f6d0e155a31
tom.yems <tom.yems@digital.homeoffice.gov.uk>
2022-07-04 14:23:11 +0100

    Fix test to look for new home page

--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -4,8 +4,8 @@ const waitForApplication = async () => {
   cy.task('log', 'Waiting for app to restart and load home page')
   cy.task('waitUntilAppRestarts')
   cy.visit('/')
-  cy.get('h1.govuk-heading-xl', { timeout: 20000 })
-    .should('contains.text', 'Prototype your service')
+  cy.get('h1.govuk-heading-l', { timeout: 20000 })
+    .should('contains.text', 'Template project')
 }
 
 module.exports = {
